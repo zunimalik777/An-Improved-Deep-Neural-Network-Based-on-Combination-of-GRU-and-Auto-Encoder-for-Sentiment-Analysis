f2cabab1e084adea3b3f03e452f5b3fe8babe062
Zulqarnain <58270466+zunimalik777@users.noreply.github.com>
2022-09-25 16:44:37 +0500

    Create Authors.py

---
+++ Authors.py
@@ -0,0 +1,9 @@
+Author:
+Muhammad Zulqarnain <zulqarnainmalik321@gmail.com>
+
+Contributors:
+Ahmed Khalaf Zager Alsaedi <ahmedkhalafzager@uomisan.edu.iq>
+Rubab Sheikh <rubabsheikh77@outlook.com>
+Irfan Javid <irfanjavid87@gmail.com>
+Maqsood Ahmad <maqsoodzee@gmail.com>
+Abaid Ullah <ubaid.aleemisb2014@gmail.com>
